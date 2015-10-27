0fab1a680bac2f96df99e8127c3d2d9f9249fc89
Bill Brookfield <billyb@tampabay.rr.com>
2015-10-26 21:06:18 -0400

    updating to use numbers

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var alexa = require('alexa-app');
 var sampleApp = new alexa.app('sample');
 
 // process the single slot supported by this skill
-sampleApp.intent('setName', function(req, res) {
-  res.card("Sample alexa-server Skill","Congratulations " + req.slot('name') + ", your skill is working");
-  res.say("Congratulations  " + req.slot('name') + ", your skill is working");
+sampleApp.intent('setNumber', function(req, res) {
+  res.card("Sample alexa-server Skill","You said " + req.slot('number') + ", your skill is working");
+  res.say("You said " + req.slot('number') + ", your skill is working");
 });
 
 
 // process launch request when no utterances detected
 sampleApp.launch(function(req, res) {
   console.log('REQUEST', JSON.stringify(req));
-  res.say("You can say, my name is Bill or my name is Joe or my name is TOP SECRET, if you work for the CIA");
+  res.say("Please say any number");
 });
 
 module.exports = function(cb) {
